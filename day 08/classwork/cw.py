a2cb774bffd6c2e3d41ea9cfdfce29b9f7d0005c
nikoloznonika <nikoloz.nonika29@gmail.com>
2025-03-31 03:43:52 +0400

    second commit

---
+++ day 08/classwork/cw.py
@@ -0,0 +1,7 @@
+num1 = 15
+num2 = 16
+print(str(15>15))
+print(str(15==15))
+print(str(15<16))
+print(str(15>=16))
+print(str(15<=16))
---
+++ day 08/classwork/cw1.py
@@ -0,0 +1,15 @@
+num1 = 15
+num2 = 16
+num3 = 17
+num4 = 18
+num5 = 19
+print(str(19>18))
+print(str(18>17))
+print(str(17>16))
+print(str(16>15))
+print(str(15==15))
+print(str(19<18))
+print(str(18<17))
+print(str(17<16))
+print(str(16<15))
+print(str(15<15))
