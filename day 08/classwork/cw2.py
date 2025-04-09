f67a41e5a455c28c52b22940716f667b0c49b760
nikoloznonika <nikoloz.nonika29@gmail.com>
2025-04-09 08:59:11 +0400

    second commit

---
+++ day 08/classwork/cw2.py
@@ -0,0 +1,11 @@
+""""```
+3) მომხმარებელს შემოატანინეთ ორი რიცხვი, თქვენი სიმაღლე და მეგობრის სიმაღლე, შემდეგ გამოიტანეთ პრინტში სამი ტექსტი ესენი არის:
+"you are taller than your friend: ", True/False(თუ მეტია თქვენი სიმაღლე True წინააღმდეგ შემთხვევაში False)
+"your friend is taller than you: ", True/False(თუ მეტია თქვენი სიმაღლე False წინააღმდეგ შემთხვევაში True)
+"You and your friend are the same height: ", True/Flase(თუ ტოლის True წინააღმდეგ შემთხვევაში False)```"""
+
+user1 = int(input("enter your ...")) 
+user2 = int(input("enter your...")) 
+
+print(user1 > user2)
+
