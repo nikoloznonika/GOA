7f7dfd07b25fbf48ef4ff7e4b2cb00de4b7e0219
nikoloznonika <nikoloz.nonika29@gmail.com>
2025-04-15 02:15:33 +0400

    second commit

---
+++ day 10/homework/hw1.py
@@ -0,0 +1,3 @@
+print(True and not False and (False or True) and True or not False)
+#პირველი სრულდება ფრჩხილებში ჩასმული მაგალიით შემდეგ სრსულდება and not შემდეგ სრულდება or not შემდეგ and 
+# ამ ოპერაციების შემდეგ მივიღებთ პასუხს
