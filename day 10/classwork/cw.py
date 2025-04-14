fcb0f496f28a19291788ebfec03ec91e07b4dca7
nikoloznonika <nikoloz.nonika29@gmail.com>
2025-04-14 08:09:12 +0400

    second commit

---
+++ day 10/classwork/cw.py
@@ -0,0 +1,3 @@
+print(True and False or False and False or True or True or False)
+#პირველი სრულდება and და მერე or  პირველში false მეორეში false მესამეში false მეოთხეში true მეხუთეში true მეექვსეში true მეშვიდეში 
+# false მერვეში troe და ა.ს.შ
